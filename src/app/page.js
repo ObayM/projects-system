64b6bc717af9b18093757c41cb32b661dbbcd646
Obay M. Rashad <102649548+ObayM@users.noreply.github.com>
2025-11-01 13:42:47 +0200

    fixed some issues, now everything is working!

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,9 @@ export default function Home(){
   return(
     <div className="min-h-screen flex justify-between items-center">
       <h1>Welcome to the best projects managemet app</h1>
-      <Link href={"/logiin"}>Login</Link>
+      <Link href={"/login"}>Login</Link>
       <Link href={"/signup"}>Login</Link>
     </div>
   );
 }
+
